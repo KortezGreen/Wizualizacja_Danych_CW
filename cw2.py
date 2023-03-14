ec1e13703f4e2e7f3c13ed5b592435a5258ae9a9
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2023-03-14 17:49:48 +0100

    Create cw2.py

---
+++ cw2.py
@@ -0,0 +1,20 @@
+#zad 1
+# import math
+# e_pow_10 = math.exp(10)
+# print(e_pow_10)
+
+#zad 2
+# name = "KACPER"
+# surname = "KIEPLIN"
+# name_formatted = name.capitalize()
+# surname_formatted = surname.capitalize()
+# print(name_formatted)
+# print(surname_formatted)
+
+#zad 3
+# piosenka = "la la la, la la la, python"
+# liczba_la = piosenka.count("la")
+# print(liczba_la)
+
+#zad 4
+
