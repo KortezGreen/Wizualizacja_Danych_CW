89c8c0998a0fdf2a86f43e1154e8b5c2cb01574c
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2023-03-14 18:46:16 +0100

    Update cw2.py

--- cw2.py
+++ cw2.py
@@ -17,4 +17,69 @@
 # print(liczba_la)
 
 #zad 4
+# text = "Pytonga robie"
+# druga = text[1]
+# ostatnia = text[-1]
+# print(druga)
+# print(ostatnia)
 
+#zad 5
+# text = "Pytonga robie"
+# podzial = text.split()
+# print(podzial)
+
+#zad 6
+# string_v = "string"
+# float_v = 21.37
+# hex_v = 0x2F
+# print("{}".format(string_v))
+# print("{}".format(float_v))
+# print("{}".format(hex_v))
+
+#zad 7
+# fav_sports = ["noga", "siata", "boks", "kosz"]
+# fav_sports.reverse()
+# fav_sports.append("golf")
+# fav_sports.append("szachy")
+# fav_sports.append("judo")
+# print(fav_sports)
+
+#zad 8
+# skroty = {"np.": "na przykład","itd.": "i tak dalej","tj.": "to jest",}
+# print(skroty)
+
+#zad 9
+# gry = {"Wiedzmin": "RPG","Frostpunk": "strategia ekonomiczna","The Forest": "survival",}
+# print(gry)
+
+#zad 10
+# zdanie = input("Podaj zdanie: ")
+# liczba_a = zdanie.count('a') + zdanie.count('A')
+# print("Liczba a: " + str(liczba_a))
+
+#zad 11
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a > b and a > c:
+#     print("A jest najwieksza")
+# elif a < b and b > c:
+#     print("B jest najwieksza")
+# elif c > a and b < c:
+#     print("C jest najwieksza")
+
+#zad 12
+# lista = [2, 3.1, 4, 4.1]
+# for i in range(len(lista)):
+#     lista[i] = lista[i]**2
+# print(lista)
+
+#zad 13
+# liczby = []
+# i = 0
+# while i<10:
+#     liczba=int(input())
+#     if liczba %2==0:
+#         liczby.append(liczba)
+#         i += 1
+#     print(liczby)
