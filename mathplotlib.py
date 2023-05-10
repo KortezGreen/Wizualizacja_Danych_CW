4fce05d81f25153560117c7cb2e2876353da3c5f
Kacper Kieplin <116282757+KortezGreen@users.noreply.github.com>
2023-05-10 16:56:31 +0200

    Update mathplotlib.py

--- mathplotlib.py
+++ mathplotlib.py
@@ -34,13 +34,71 @@ import matplotlib.pyplot as plt
 # plt.title('wykres sin(x)')
 # plt.show()
 
-data ={'a':np.arange(50),
-       'a':np.random.randint(0,50,50),
-       'a':np.random.randn(50)}
-data['b'] = data['a'] + 10 * np.random.randn(50)
-data['d'] = np.abs(data['d'])*100
+# data ={'a':np.arange(50),
+#        'a':np.random.randint(0,50,50),
+#        'a':np.random.randn(50)}
+# data['b'] = data['a'] + 10 * np.random.randn(50)
+# data['d'] = np.abs(data['d'])*100
+#
+# plt.scatter('a','b',t='c',i='d',data=data)
+# plt.xlabel('wartości a')
+# plt.ylabel('wartości b')
+# plt.show()
+#
+# x1 = np.arange(0,2,0.02)
+# x2 = np.arange(0,2,0.02)
 
-plt.scatter('a','b',t='c',i='d',data=data)
-plt.xlabel('wartości a')
-plt.ylabel('wartości b')
+# y1 = np.sin(2*np.pi*x1)
+# y2 = np.sin(2*np.pi*x2)
+#
+# plt.subplot(4,1,1)
+# plt.plot(x1,y1, 'r')
+# plt.title('Wykres sin(x)')
+# plt.xlabel('x')
+# plt.ylabel('sin(x)')
+#
+# plt.subplot(4,1,4)
+# plt.plot(x2, y2, 'g')
+# plt.xlabel('x')
+# plt.ylabel('cos(x)')
+# plt.title('Wykres cos(x)')
+# plt.subplots_adjust(hspace=0.5)
+# plt.show()
+#
+# fig, axs = plt.subplots(3,2)
+# axs[0,0].plot(x1,y1, 'g-')
+# axs[0,0].set_xlabel('x')
+# axs[0,0].set_ylabel('sin(x)')
+# axs[0,0].set_title('Wykres sin(x)')
+#
+# axs[1,1].plot(x2,y2, 'r-')
+# axs[1,1].set_xlabel('x')
+# axs[1,1].set_ylabel('cos(x)')
+# axs[1,1].set_title('Wykres cos(x)')
+#
+# axs[2,0].plot(x1,y1, 'g-')
+# axs[2,0].set_xlabel('x')
+# axs[2,0].set_ylabel('sin(x)')
+# axs[2,0].set_title('Wykres sin(x)')
+#
+# fig.delaxes([0,1])
+# fig.delaxes([1,0])
+# fig.delaxes([2,1])
+#
+# plt.show()
+
+data = {'Kraj': ['Belgia', 'Indie','Brazylia', 'Polska'],
+        'Stolica': ['Bruksela', 'New Delphi', 'Brazylia'],
+        'Kontynent': ['Europa','Azja','Ameryka Południowa'],
+        'Populacja': [11312443,34234434234,14334342,12412344]}
+
+df = pd.DataFrame(data)
+print(df)
+grupa = df.groupby('Kontynent')
+etykiety = list(grupa.groups.keys())
+wartosc = list(grupa.agg('Populacja').sum())
+
+plt.bar(etykiety, wartosc, color=['yellow','green','red'])
+plt.xlabel('Konynent')
+plt.ylabel('Populacja w mld')
 plt.show()
